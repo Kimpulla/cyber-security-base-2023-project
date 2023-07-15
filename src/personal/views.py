7b08d293d3403e1c6914a3e139d1a380065642b2
Kimpulla <haukka.kimi@gmail.com>
2023-07-15 23:18:45 +0300

    updated views

--- src/personal/views.py
+++ src/personal/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-    print(request.headers)
-    return render(request, "base.html", {})
+    
+    context= {}
+    context['some_string'] = "this is some string that I'm passing to the view"
+
+    return render(request, "personal/home.html", context)
