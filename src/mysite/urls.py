9153777b2f4e7f18778fd8f4d7ba274257ae3884
Kimpulla <haukka.kimi@gmail.com>
2023-07-15 21:32:24 +0300

    edited urls

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from personal.views import (home_screen_view)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_screen_view, name="home"),
+
 ]
