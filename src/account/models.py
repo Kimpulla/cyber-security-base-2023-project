f71d77b9ff8d7d65a958b91bf88286645ce5b788
Kimpulla <haukka.kimi@gmail.com>
2023-07-19 21:08:43 +0300

    fixed typo

--- src/account/models.py
+++ src/account/models.py
@@ -46,11 +46,13 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
-    def __str__(self):
-        return self.email
-    
-    def has_perm(self, perm, obj=None):
-	    return self.is_admin
+    objects = MyAccountManager()
 
-    def has_module_perms(self, app_label):
-        return True
+def __str__(self):
+    return self.email
+    
+def has_perm(self, perm, obj=None):
+	return self.is_admin
+
+def has_module_perms(self, app_label):
+    return True
