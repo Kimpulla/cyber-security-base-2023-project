6c1b12a4a1bb78e9f431d396a4b0a967ee18bcf4
Kimpulla <haukka.kimi@gmail.com>
2023-07-27 21:48:05 +0300

    over all styling

--- src/account/models.py
+++ src/account/models.py
@@ -1,58 +1,72 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Manage situation when new user is created
+
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
-        if not email:
-            raise ValueError("Email is required")
-        if not username:
-            raise ValueError("Username is required")
-          
-        user = self.model(
-            email=self.normalize_email,
-            username=username,
-        )
+	def create_user(self, email, username, password=None):
+		if not email:
+			raise ValueError('Users must have an email address')
+		if not username:
+			raise ValueError('Users must have a username')
 
-        user.set_password(password)
-        user.save(user=self._db)
-        return user
+		user = self.model(
+			email=self.normalize_email(email),
+			username=username,
+		)
 
-    def create_superuser(self, email, username, password=None):
-        user = self.model(
-            email=self.normalize_email,
-            username=username,
-            password=password,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
 
+	def create_superuser(self, email, username, password):
+		user = self.create_user(
+			email=self.normalize_email(email),
+			password=password,
+			username=username,
+		)
+		user.is_admin = True
+		user.is_staff = True
+		user.is_superuser = True
+		user.save(using=self._db)
+		return user
 
 
 class Account(AbstractBaseUser):
-    email                   = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    username                = models.CharField(max_length=30, unique=True)
-    date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin				= models.BooleanField(default=False)
-    is_active				= models.BooleanField(default=True)
-    is_staff				= models.BooleanField(default=False)
-    is_superuser			= models.BooleanField(default=False)
+	email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
+	username 				= models.CharField(max_length=30, unique=True)
+	date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+	last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
+	is_admin				= models.BooleanField(default=False)
+	is_active				= models.BooleanField(default=True)
+	is_staff				= models.BooleanField(default=False)
+	is_superuser			= models.BooleanField(default=False)
 
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
-    objects = MyAccountManager()
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
 
-def __str__(self):
-    return self.email
-    
-def has_perm(self, perm, obj=None):
-	return self.is_admin
+	objects = MyAccountManager()
 
-def has_module_perms(self, app_label):
-    return True
+	def __str__(self):
+		return self.email
+
+	# For checking permissions. to keep it simple all admin have ALL permissons
+	def has_perm(self, perm, obj=None):
+		return self.is_admin
+
+	# Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
+	def has_module_perms(self, app_label):
+		return True
+
+
+
+
+
+
+
+
+
+
+
+
+
