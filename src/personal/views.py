bd8590a3a7dfa7748dee3c0d2cba4ad985a6c78e
Kimpulla <haukka.kimi@gmail.com>
2023-07-30 21:12:48 +0300

    updated home_screen_view and added some comments.

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render
-from account.models import Account
+from operator import attrgetter
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Create your views here.
+# Import functions and models from the 'blog' app
+from blog.views import get_blog_queryset
+from blog.models import BlogPost
 
-def home_screen_view(request):
-    
-    context= {}
+# Define the number of blog posts to display per page
+BLOG_POSTS_PER_PAGE = 10
 
-    accounts = Account.objects.all()
-    context['accounts'] = accounts
+# View function for the home screen
+def home_screen_view(request, *args, **kwargs):
+	
+	context = {}
 
-    return render(request, "personal/home.html", context)
+    # Get the search query from the request
+	query = ""
+	if request.GET:
+		query = request.GET.get('q', '')
+		context['query'] = str(query)
+
+	# Get the list of blog posts based on the search query, sorted by date_updated in descending order
+	blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+	
+	# Pagination
+	page = request.GET.get('page', 1)
+	blog_posts_paginator = Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
+	try:
+		blog_posts = blog_posts_paginator.page(page)
+	except PageNotAnInteger:
+		# If the page parameter is not an integer, display the first page
+		blog_posts = blog_posts_paginator.page(BLOG_POSTS_PER_PAGE)
+		
+	except EmptyPage:
+	# If the page is out of range (e.g., too high), display the last page
+		blog_posts = blog_posts_paginator.page(blog_posts_paginator.num_pages)
+
+	context['blog_posts'] = blog_posts
+
+    # Render the "personal/home.html" template with the context data
+	return render(request, "personal/home.html", context)
+
+
+
