22c1196050693a58fb9b0c1c0bcf556e84584981
Kimpulla <haukka.kimi@gmail.com>
2023-07-31 01:02:24 +0300

    Broken Authentication vulnerability! Weak password policy, user can register with simple and known passwords.

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -101,15 +101,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
+# Bad validation
+# Broken Authentication
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
